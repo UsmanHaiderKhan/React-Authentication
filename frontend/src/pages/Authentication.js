d1bc91d580558d136e3e696936ea3cd754503b2e
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-08-11 21:26:46 +0200

    🎉 Add : authentication page with login/signup logic
    Introduces AuthenticationPage and an action handler for login and signup. Handles form submission, communicates with backend, stores token and expiration in localStorage, and redirects on success.

---
+++ frontend/src/pages/Authentication.js
@@ -0,0 +1,48 @@
+import AuthForm from '../components/AuthForm';
+import {redirect} from "react-router-dom";
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+export async function action({ request, params }) {
+    const searchParams = new URL(request.url).searchParams;
+    const mode = searchParams.get('mode') || 'login';
+
+    if (mode !== 'login' && mode !== 'signup') {
+        throw new Error('Unsupported mode!');
+    }
+
+    const data = await request.formData();
+    const authData = {
+        email: data.get('email'),
+        password: data.get('password'),
+    };
+
+    const response = await fetch(
+        `http://localhost:8080/${mode === 'login' ? 'login' : 'signup'}`,
+        {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(authData),
+        }
+    );
+
+    if (response.status === 422 || response.status === 401) {
+        return response;
+    }
+
+    if (!response.ok) {
+        throw new Error('Could not authenticate user.');
+    }
+
+    const resData = await response.json();
+
+    localStorage.setItem('token', resData.token);
+    localStorage.setItem('expiration', resData.expiration);
+
+    return redirect('/');
+}
